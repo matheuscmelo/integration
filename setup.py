0158d6754488037e9fa8c6a8c651efe6f4f131c5
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-02-22 15:58:23 -0300

    Adding setup and tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Asperathos Integration Tests',
+      version='1.0',
+      description='Integration tests for Asperathos',
+      author='Matheus Melo',
+      author_email='matheuscmacc@gmail.com',
+      url='',
+     )
